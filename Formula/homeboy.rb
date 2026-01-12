b782ed7b6c3acef5188c3b3e2e4ae9a9a6bbda1b
Chris Huber <chubes@extrachill.com>
2026-01-12 14:49:48 +0000

    homeboy 0.3.0

--- Formula/homeboy.rb
+++ Formula/homeboy.rb
@@ -1,25 +1,25 @@
 class Homeboy < Formula
   desc "CLI tool for development and deployment automation"
   homepage "https://github.com/Extra-Chill/homeboy-cli"
-  version "0.2.19"
+  version "0.3.0"
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/Extra-Chill/homeboy-cli/releases/download/v0.2.19/homeboy-aarch64-apple-darwin.tar.xz"
-      sha256 "0d3c73c28bcefbb357a95e2c7e36809d0dedb0b1515dc2fc00492be67b297fc7"
+      url "https://github.com/Extra-Chill/homeboy-cli/releases/download/v0.3.0/homeboy-aarch64-apple-darwin.tar.xz"
+      sha256 "824a67b790055f7fba8c35cc06ee86e2ea6fcbb0028d68ece1313efc255b76ca"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/Extra-Chill/homeboy-cli/releases/download/v0.2.19/homeboy-x86_64-apple-darwin.tar.xz"
-      sha256 "a03d0937013c3c31135261a11897790ef0ba85f6792fc043adc5924fd50c570c"
+      url "https://github.com/Extra-Chill/homeboy-cli/releases/download/v0.3.0/homeboy-x86_64-apple-darwin.tar.xz"
+      sha256 "e75ddd315db0bdbb0e081951fc0a1964cf2884861e0c3ddcea08383125c29fc9"
     end
   end
   if OS.linux?
     if Hardware::CPU.arm?
-      url "https://github.com/Extra-Chill/homeboy-cli/releases/download/v0.2.19/homeboy-aarch64-unknown-linux-gnu.tar.xz"
-      sha256 "0993d2240001aee51070b7fee62dd0a7a59605eb1b193a73c5f428db622c1394"
+      url "https://github.com/Extra-Chill/homeboy-cli/releases/download/v0.3.0/homeboy-aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "c2c94cfe6b30fa7810b9efec4ce8920c442a96cf383fdc5555af17d0277e8a13"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/Extra-Chill/homeboy-cli/releases/download/v0.2.19/homeboy-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "f839889386005045413f7174b9a25faa8f5d5fdc422d690d25701c6bcbd58f55"
+      url "https://github.com/Extra-Chill/homeboy-cli/releases/download/v0.3.0/homeboy-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "e2a203c1383b6334ea6241c0ee5fa0684239aeba4a7eb07665644d603def4655"
     end
   end
   license "MIT"
